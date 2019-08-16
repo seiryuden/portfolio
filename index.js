985af547d2ef5214740420ee75a60dfee20e942b
seiryuden <roberto.il@outlook.com>
2019-08-16 12:46:54 +0900

    v2.0 new portfolio

--- index.js
+++ index.js
@@ -25,30 +25,21 @@ function work(){
             </div>
             <div class="description-cont">
                 <h1 class="project-name">VGP Monitor</h1>
-                <p class="description">An app for fetching and displaying news and other online content related to major video game platforms. It provides a quick overview of material published by popular websites (e.g. Gamespot, Youtube) in the specified date. Built with HTML, CSS, JavaScript and JQuery.</p>
+                <p class="description">An app for fetching news related to major video game platforms from popular websites. Built with HTML, CSS, JavaScript and JQuery.</p>
                 <a href="https://github.com/seiryuden/vgp-monitor"><img src="img/github.png" class="repo" alt="github logo"></a>
             </div>
         </section>
 
         <section>
-            <div class="thumb-cont">
+            <div class="thumb-cont placeholder">
                 
             </div>
             <div class="description-cont">
-                <h1 class="project-name">Project Name</h1>
+                <h1 class="project-name">ReactJS project for language learning - Coming soon</h1>
                 <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus nunc a neque mattis, quis maximus enim tincidunt. Sed maximus velit ligula.</p>
             </div>
         </section>
 
-        <section>
-            <div class="thumb-cont">
-                
-            </div>
-            <div class="description-cont">
-                <h1 class="project-name">Project Name</h1>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus nunc a neque mattis, quis maximus enim tincidunt. Sed maximus velit ligula.</p>  
-            </div>
-        </section>
     
     </div>
     
@@ -58,11 +49,12 @@ function work(){
 function about(){
     return `
     <div class="wrap-about">
-        <p class="about-text">I'm passionate about crafting appealing and highly-polished user interfaces. In my work I strive for quality not only because of its economic value, but because I derive self-realization and meaning from it.  
+        <div class="js-header"><img class="robpic" src="img/rob.jpg"></img></div>
+        <p class="about-text">I'm passionate about crafting appealing and highly polished user interfaces. In my work I strive for quality not only as a means for economic value, but also as a source of self-realization.
         </p>
-        <p class="about-text">I learned front-end web development at <a href="https://www.thinkful.com/"> Thinkful's</a> full-stack bootcamp, where I mastered skills to build websites using HTML, CSS, JavaScript, JQuery and ReactJS. Currently I'm also studying back-end technologies such as Node and Postgres. Apart from my technical skills, during my years working as a lawyer I’ve been recognized as highly reliable and cooperative, capable of working under pressure and of maintaining great interpersonal relationships. 
+        <p class="about-text">I learned front-end web development at <a href="https://www.thinkful.com/"> Thinkful's</a> full-stack bootcamp, and I'm currently studying back-end technologies such as Node and Postgres. Before doing web development I worked in Chile for several years as a lawyer, mostly in the areas of contract law, corporate law and public procurement.  
         </p>
-        <p class="about-text">When I’m not coding, I’m usually studying Japanese, hiking, spending time with friends or playing video games (I wish I had more time for them!).</p>
+        <p class="about-text">When I’m not coding, I’m usually studying Japanese, hiking or spending time with friends.</p>
 
         <p>Languages: English (fluent), Spanish (native) and Japanese (intermediate).</p>
     </div>
@@ -86,7 +78,7 @@ function contact(){
 function renderWork(){
     console.log("renderWork ran");
     $(".js-work").click(function(event){
-
+        
         $(".js-content").empty();
         $(".js-content").html(work());
     });
@@ -95,8 +87,10 @@ function renderWork(){
 
 function renderAbout(){
     console.log("renderAbout ran");
-    $(".js-about").click(function(event){
 
+
+    $(".js-about").click(function(event){
+       
         $(".js-content").empty();
         $(".js-content").html(about());
     });    
@@ -106,7 +100,6 @@ function renderContact(){
     console.log("renderContact ran");
 
     $(".js-contact").click(function(event){
-
         $(".js-content").empty();
         $(".js-content").html(contact());
     })
